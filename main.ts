b6b3f7f1725bb250f69f36300d6f1e897623e1d4
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2021-06-10 13:51:10 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ function kiSteuerung () {
         Ausgabe_String = "" + Ausgabe_String + snapshot[Index] + ","
     }
     serial.writeLine(Ausgabe_String)
-    nntest.predict(snapshot, ergebnis)
+    // nntest.predict(snapshot, ergebnis)
     if (ergebnis[0] >= 0.5) {
         output = "A"
         buttonA()
@@ -60,8 +60,32 @@ let snapshot: number[] = []
 let Ausgabe_String = ""
 let ergebnis: number[] = []
 let isConnected = 0
+let car = null
 ergebnis = [0, 0, 0]
-nntest.fcnnfromjson("{\"params\":[{\"layers\":[6,7,7,3]},{\"act\":\"relu\"}],\"coefs\":[[[2.044017,-2.722454,2.000679,-1.514816,0.333606,-2.761747,-0.0],[0.028879,0.096323,-0.24589,0.015551,0.00977,0.001861,0.0],[-0.070218,-0.472358,0.379994,-0.024475,0.003899,-0.393389,0.0],[-0.74473,-0.013117,0.923773,0.1172,0.654482,0.33238,-0.0],[-0.03287,0.362568,0.806845,-0.332245,-0.198489,0.397769,0.0],[0.02064,0.011437,-0.209757,0.022583,0.077942,-0.015708,0.0]],[[-0.0,0.563227,-0.0,-0.196192,-2.71285,2.666878,1.751509],[0.0,-0.0,-0.0,1.679954,1.483361,-2.497833,-0.268884],[-0.0,0.118874,-0.0,-1.440332,-0.33425,0.058711,0.811402],[0.0,-5.311466,0.0,-0.468864,0.057601,-0.743952,1.867721],[-0.0,0.394793,-0.0,-0.382019,-0.52901,3.864352,1.189382],[0.0,-6.229019,0.0,2.250281,1.383958,2.699693,0.394597],[-0.0,-0.0,-0.0,-0.0,-0.0,0.0,-0.0]],[[-0.0,-0.0,0.0],[1.111976,2.797596,-2.238281],[0.0,-0.0,-0.0],[-6.529347,0.144358,3.030409],[3.251293,1.944163,-2.400912],[1.324108,-3.076313,1.005805],[-1.958187,-0.03444,2.36584]]],\"intercepts\":[[-0.449843,0.772355,-0.254264,1.384457,-0.668279,1.448058,-0.658712],[-0.591404,1.820663,-0.630498,0.797055,1.109102,0.036825,-1.220646],[-0.320211,1.158411,-1.175748]]}")
+basic.forever(function () {
+    basic.pause(100)
+    if (gameOn == true) {
+        car5 = game.createSprite(4, 0)
+        basic.pause(Math.randomRange(0, 5001))
+        while (gameOn == true) {
+            if (car5.get(LedSpriteProperty.Y) == 4) {
+                if (playerCar.isTouching(car5)) {
+                    gameOn = false
+                } else {
+                    score = score + 1
+                    car5.delete()
+                    basic.pause(Math.randomRange(0, 5001))
+                    car5 = game.createSprite(4, 0)
+                    basic.pause(500)
+                }
+            } else {
+                car5.change(LedSpriteProperty.Y, 1)
+                basic.pause(500)
+            }
+        }
+    }
+})
+// nntest.fcnnfromjson("{\"params\":[{\"layers\":[6,7,7,3]},{\"act\":\"relu\"}],\"coefs\":[[[2.044017,-2.722454,2.000679,-1.514816,0.333606,-2.761747,-0.0],[0.028879,0.096323,-0.24589,0.015551,0.00977,0.001861,0.0],[-0.070218,-0.472358,0.379994,-0.024475,0.003899,-0.393389,0.0],[-0.74473,-0.013117,0.923773,0.1172,0.654482,0.33238,-0.0],[-0.03287,0.362568,0.806845,-0.332245,-0.198489,0.397769,0.0],[0.02064,0.011437,-0.209757,0.022583,0.077942,-0.015708,0.0]],[[-0.0,0.563227,-0.0,-0.196192,-2.71285,2.666878,1.751509],[0.0,-0.0,-0.0,1.679954,1.483361,-2.497833,-0.268884],[-0.0,0.118874,-0.0,-1.440332,-0.33425,0.058711,0.811402],[0.0,-5.311466,0.0,-0.468864,0.057601,-0.743952,1.867721],[-0.0,0.394793,-0.0,-0.382019,-0.52901,3.864352,1.189382],[0.0,-6.229019,0.0,2.250281,1.383958,2.699693,0.394597],[-0.0,-0.0,-0.0,-0.0,-0.0,0.0,-0.0]],[[-0.0,-0.0,0.0],[1.111976,2.797596,-2.238281],[0.0,-0.0,-0.0],[-6.529347,0.144358,3.030409],[3.251293,1.944163,-2.400912],[1.324108,-3.076313,1.005805],[-1.958187,-0.03444,2.36584]]],\"intercepts\":[[-0.449843,0.772355,-0.254264,1.384457,-0.668279,1.448058,-0.658712],[-0.591404,1.820663,-0.630498,0.797055,1.109102,0.036825,-1.220646],[-0.320211,1.158411,-1.175748]]}")
 basic.forever(function () {
     basic.pause(100)
     if (gameOn == true) {
@@ -88,21 +112,21 @@ basic.forever(function () {
 basic.forever(function () {
     basic.pause(100)
     if (gameOn == true) {
-        car5 = game.createSprite(4, 0)
+        car3 = game.createSprite(2, 0)
         basic.pause(Math.randomRange(0, 5001))
         while (gameOn == true) {
-            if (car5.get(LedSpriteProperty.Y) == 4) {
-                if (playerCar.isTouching(car5)) {
+            if (car3.get(LedSpriteProperty.Y) == 4) {
+                if (playerCar.isTouching(car3)) {
                     gameOn = false
                 } else {
                     score = score + 1
-                    car5.delete()
+                    car3.delete()
                     basic.pause(Math.randomRange(0, 5001))
-                    car5 = game.createSprite(4, 0)
+                    car3 = game.createSprite(2, 0)
                     basic.pause(500)
                 }
             } else {
-                car5.change(LedSpriteProperty.Y, 1)
+                car3.change(LedSpriteProperty.Y, 1)
                 basic.pause(500)
             }
         }
@@ -154,37 +178,6 @@ basic.forever(function () {
         }
     }
 })
-basic.forever(function () {
-    basic.pause(100)
-    if (gameOn == true) {
-        car3 = game.createSprite(2, 0)
-        basic.pause(Math.randomRange(0, 5001))
-        while (gameOn == true) {
-            if (car3.get(LedSpriteProperty.Y) == 4) {
-                if (playerCar.isTouching(car3)) {
-                    gameOn = false
-                } else {
-                    score = score + 1
-                    car3.delete()
-                    basic.pause(Math.randomRange(0, 5001))
-                    car3 = game.createSprite(2, 0)
-                    basic.pause(500)
-                }
-            } else {
-                car3.change(LedSpriteProperty.Y, 1)
-                basic.pause(500)
-            }
-        }
-    }
-})
-control.inBackground(function () {
-    while (true) {
-        basic.pause(200)
-        if (gameOn == true) {
-            kiSteuerung()
-        }
-    }
-})
 control.inBackground(function () {
     score = 0
     playerCar = game.createSprite(2, 4)
@@ -194,4 +187,12 @@ control.inBackground(function () {
     }
     game.addScore(score)
     game.gameOver()
+})
+control.inBackground(function () {
+    while (true) {
+        basic.pause(200)
+        if (gameOn == true) {
+            kiSteuerung()
+        }
+    }
 })
